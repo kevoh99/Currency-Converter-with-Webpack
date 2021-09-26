5746d585e60d648ff675a87c1dd9b5cc094fad5b
Kevin Njoroge <njorogekevin.njoroge@gmail.com>
2021-09-26 11:54:10 +0300

    Combined both tutorials, about to start implementing the uncompleted changes as shown in the NOTE!!!!! file

---
+++ src/vendor.js
@@ -0,0 +1,2 @@
+// This file is meant for importing external vendor libraries such as bootstrap, jquery, etc. The idea is to separate these with the main code as they change less frequently.
+window.alert('Hi from vendor')

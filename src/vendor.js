ebe13f539b9e9d5c8d944f045617026590aa4c0d
Kevin Njoroge <njorogekevin.njoroge@gmail.com>
2021-10-05 14:23:21 +0300

    Added fontawesome webpack compatibility
    - Fontawesome icons were previously not loading correctly
    - Also added CSS to make the icons white in color

--- src/vendor.js
+++ src/vendor.js
@@ -1,2 +1,7 @@
 // This file is meant for importing external vendor libraries such as bootstrap, jquery, etc. The idea is to separate these with the main code as they change less frequently.
 // window.alert('Hi from vendor.js, home of external libraries')
+// Import Fontawesome
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
